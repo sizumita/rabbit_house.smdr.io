dc3fb38179f93fb36748441a980fe7e9a6c0d1cb
Sumito Izumita <sumito@izumita.com>
2023-12-25 23:28:07 +0900

    Update tinc-up

--- create-certs.sh
+++ create-certs.sh
@@ -1,9 +1,10 @@
+rm -rf ansible/files/certificate/strongSwan/ca-key.pem ansible/files/certificate/strongSwan/ca-cert.pem ansible/files/certificate/strongSwan/server-key.pem ansible/files/certificate/strongSwan/server-cert.pem
 sudo ipsec pki --gen --type rsa --size 4096 --outform pem > ./ansible/files/certificate/strongSwan/ca-key.pem
 sudo ipsec pki --self --ca --lifetime 3650 --in ./ansible/files/certificate/strongSwan/ca-key.pem --type rsa --dn "CN=Rabbit House VPN root CA" --outform pem > ./ansible/files/certificate/strongSwan/ca-cert.pem
 
 sudo ipsec pki --gen --type rsa --size 4096 --outform pem > ./ansible/files/certificate/strongSwan/server-key.pem
-pki --in ./ansible/files/certificate/strongSwan/server-key.pem --type rsa \
-  | pki --issue --lieftime 1825 \
+sudo ipsec pki --pub --in ./ansible/files/certificate/strongSwan/server-key.pem --type rsa \
+  | pki --issue --lifetime 1825 \
   --cacert ./ansible/files/certificate/strongSwan/ca-cert.pem \
   --cakey ./ansible/files/certificate/strongSwan/ca-key.pem \
   --dn "CN=rabbit_house.smdr.io" --san rabbit_house.smdr.io \
