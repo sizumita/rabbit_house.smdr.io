7e64308baa3a1e4856ef97e83089208e9079df60
Sumito Izumita <sumito@izumita.com>
2023-12-25 23:54:51 +0900

    Add creating client cert

--- create-certs.sh
+++ create-certs.sh
@@ -1,4 +1,4 @@
-rm -rf ansible/files/certificate/strongSwan/ca-key.pem ansible/files/certificate/strongSwan/ca-cert.pem ansible/files/certificate/strongSwan/server-key.pem ansible/files/certificate/strongSwan/server-cert.pem
+rm -rf ansible/files/certificate/strongSwan/*.pem
 sudo ipsec pki --gen --type rsa --size 4096 --outform pem > ./ansible/files/certificate/strongSwan/ca-key.pem
 sudo ipsec pki --self --ca --lifetime 3650 --in ./ansible/files/certificate/strongSwan/ca-key.pem --type rsa --dn "CN=Rabbit House VPN root CA" --outform pem > ./ansible/files/certificate/strongSwan/ca-cert.pem
 
@@ -10,3 +10,19 @@ sudo ipsec pki --pub --in ./ansible/files/certificate/strongSwan/server-key.pem 
   --dn "CN=rabbit-house.smdr.io" --san rabbit-house.smdr.io \
   --flag serverAuth --flag ikeIntermediate --outform pem \
   > ./ansible/files/certificate/strongSwan/server-cert.pem
+
+ipsec pki --gen --type rsa --size 2048 --outform pem > ./ansible/files/certificate/strongSwan/client-key.pem
+ipsec pki --pub --in ./ansible/files/certificate/strongSwan/client-key.pem --type rsa \
+  | ipsec pki --issue --lifetime 730 --outform pem \
+  --cacert ./ansible/files/certificate/strongSwan/ca-cert.pem \
+  --cakey ./ansible/files/certificate/strongSwan/ca-key.pem \
+  --dn "C=CH, O=strongSwan, CN=sumito@izumita.com" \
+  --san sumito@izumita.com \
+  > ./ansible/files/certificate/strongSwan/client-cert.pem
+
+openssl pkcs12 -export -name "Rabbit House VPN Certificate" \
+  -inkey ./ansible/files/certificate/strongSwan/client-key.pem \
+  -in ./ansible/files/certificate/strongSwan/client-cert.pem \
+  -certfile ./ansible/files/certificate/strongSwan/ca-cert.pem \
+  -caname "Rabbit House VPN root CA" \
+  -out Client.p12
