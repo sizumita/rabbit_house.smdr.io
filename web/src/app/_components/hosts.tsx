7613e31d088f2affa41283848bf8a8cee5f293b5
sizumita <github@smdr.io>
2024-01-08 02:12:35 +0900

    Fix layout

--- web/src/app/_components/hosts.tsx
+++ web/src/app/_components/hosts.tsx
@@ -6,27 +6,29 @@ import Link from "next/link";
 export const Hosts = () => {
     const hosts = useHosts()
 
-    return <Container className={"sm:col-span-2"}>
-        <div className={"min-h-32"}>
-            <h2 className={"font-bold text-2xl mb-2"}>GSNetに存在しているノードの状態</h2>
-            <table className={"text-lg table-fixed w-full border-collapse border border-orange-500 rounded-md"}>
-                <thead>
-                <tr>
-                    <th className={"border border-orange-500"}>IP</th>
-                    <th className={"border border-orange-500"}>状態</th>
-                </tr>
-                </thead>
-                <tbody>
-                {hosts.map((host, i) => <tr key={i}>
-                    <td className={"border border-orange-500 px-2"}>
-                        <Link href={`http://${host.ip}`} className={"font-bold"}>
-                            {host.ip}
-                        </Link>
-                    </td>
-                    <td className={"border border-orange-500 px-2"}>{host.up ? "起動" : "停止"}</td>
-                </tr>)}
-                </tbody>
-            </table>
-        </div>
-    </Container>
+    return <div className={"grid grid-cols-subgrid gap-4 col-span-1 sm:col-span-2 lg:col-span-4"}>
+        <Container className={"sm:col-span-2"}>
+            <div className={"min-h-32"}>
+                <h2 className={"font-bold text-2xl mb-2"}>GSNetに存在しているノードの状態</h2>
+                <table className={"text-lg table-fixed w-full border-collapse border border-orange-500 rounded-md"}>
+                    <thead>
+                    <tr>
+                        <th className={"border border-orange-500"}>IP</th>
+                        <th className={"border border-orange-500"}>状態</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {hosts.map((host, i) => <tr key={i}>
+                        <td className={"border border-orange-500 px-2"}>
+                            <Link href={`http://${host.ip}`} className={"font-bold"}>
+                                {host.ip}
+                            </Link>
+                        </td>
+                        <td className={"border border-orange-500 px-2"}>{host.up ? "起動" : "停止"}</td>
+                    </tr>)}
+                    </tbody>
+                </table>
+            </div>
+        </Container>
+    </div>
 }
